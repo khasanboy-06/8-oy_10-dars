48f5b4b0f74ef92992fd7ff816a032cf10bab5e9
Hasanboy Nasirdinov <164983145+khasanboy-06@users.noreply.github.com>
2024-07-13 13:55:43 +0500

    10-dars

--- config/urls.py
+++ config/urls.py
@@ -16,16 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from config_app.views import VideoView
+from config_app.views import VideoCreateView, VideoUpdateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/video/', VideoView.as_view()),
+    path('api/v1/video/', VideoCreateView.as_view()),
+    path('api/v1/video/<int:pk>/', VideoUpdateView.as_view()),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('api-auth/', include('rest_framework.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- config_app/views.py
+++ config_app/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 
 from rest_framework import generics
-from rest_framework import authentication
 
 from .models import Video
 from .serializers import VideoSerializer
+from .permissions import VideoPermission
 
-class VideoView(generics.ListCreateAPIView):
+class VideoCreateView(generics.ListCreateAPIView, generics.ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+    permission_classes = [VideoPermission]
 
 
-    
+class VideoUpdateView(generics.RetrieveUpdateAPIView, generics.RetrieveUpdateDestroyAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    permission_classes = [VideoPermission]
